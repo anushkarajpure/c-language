47058c46b2d9c844f8219e1cf9a178e7e8dd0558
anushkarajpure <anushkarajpure802@gmail.com>
2024-08-22 18:57:11 +0530

    Add files via upload

---
+++ pr.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	char grade;
+	int mark;
+	
+	printf("enter the marks:"); 
+	scanf("%d",&mark);
+	 
+	grade=(mark>=90&&mark<=100)?'A'
+	     :(mark>=80&&mark<90)?'B'
+	     :(mark>=70&&mark<80)?'C'
+	     :(mark>=60&&mark<70)?'D'
+	     :(mark>=40&&mark<60)?'E'
+	     :'F';
+	     
+	printf("your grade is %c:",grade);     
+}
